b7636666fbfb6422231dd2023b8af1cc7d718eab
Jakub Sowa <sowa.jakub@gmail.com>
2017-09-07 10:28:58 +0200

    initial setup

---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '10.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+source 'https://github.com/CocoaPods/Specs'
+source 'https://github.com/Appnroll/Specs'
+
+target 'Sunny' do
+    pod 'RxSwift', '~> 3.5'
+    pod 'RxCocoa', '~> 3.5'
+
+    target 'SunnyTests' do
+        pod 'Quick', '~> 1.1'
+        pod 'Nimble', '~> 7.0'
+    end
+end
---
+++ Sunny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+//  Created by Jakub Sowa on 07.09.2017.
+//  Copyright © 2017 com.owlyapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
